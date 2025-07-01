00d0489c38d1dca388d4b1d033bbe33a60677cd8
Rishi <dramawave7@gmail.com>
2025-07-01 10:37:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "24890303"))
+API_HASH     = environ.get("API_HASH", "94cf78d1e6883ecb10f32e31fc23cfe0")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002664392843"))
+ADMIN        = int(environ.get("ADMIN", "7313000783"))
+CHANNEL      = environ.get("CHANNEL", "@KdramaTalkies")
